54df0cc795270cd8a89e5ae3e0eb16e62f6e64f6
Preethampree <lakshmipreetham17@gmail.com>
2025-09-09 21:54:59 +0530

    Added Hello World API (GET /hello)

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class HelloController {
   @Get()
   getHello() {
-    return { message: 'Hello World' };
+    return { message: 'Hello World api' };
   }
 }
